116f8a708d6f9a87572bb9ff99335115ae793a70
muhammmadarab <muhammadarab781@gmail.com>
2021-06-26 14:22:02 +0530

    feature update
    users can now mark  important to notes. updated the delete function and delete all notes function. users will not be able to add empty notes anymore

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,89 +1,104 @@
+// window.addEventListener("load", function () {
+//   console.clear();
+// });
+
 showNotes();
-darkMode();
 
 // add note function
 let addNoteBtn = document.getElementById("addNoteBtn");
 addNoteBtn.addEventListener("click", function (e) {
-  let noteInput = document.getElementById("noteTextInput");
   let noteTitle = document.getElementById("noteTitleInput");
-  let note = localStorage.getItem("note");
+  let noteInput = document.getElementById("noteTextInput");
+  let imp = document.getElementById("impSwitch");
+  let notes = localStorage.getItem("notes");
 
-  if (note == null) {
-    notesObj = [];
+  if (noteInput.value.length == 0) {
+    alert("Note cannot be empty");
   } else {
-    // notesObj = '{ "title": "noteTitle.value", "noteV": "noteInput.value" };';
-    notesObj = JSON.parse(note);
+    if (notes == null) {
+      notesObj = [];
+    } else {
+      notesObj = JSON.parse(notes);
+    }
+
+    notesObj.push({
+      title: noteTitle.value,
+      note: noteInput.value,
+      imp: imp.checked,
+    });
+
+    localStorage.setItem("notes", JSON.stringify(notesObj));
+
+    noteTitle.value = "";
+    noteInput.value = "";
+    imp.checked = false;
+
+    showNotes();
+    darkMode();
   }
-
-  notesObj.push(noteTitle.value, noteInput.value);
-  localStorage.setItem("note", JSON.stringify(notesObj));
-  noteInput.value = "";
-  noteTitle.value = "";
-
-  showNotes();
-  darkMode();
 });
 
 // function to show notes
 function showNotes() {
-  let note = localStorage.getItem("note");
+  let note = localStorage.getItem("notes");
+  let imp = localStorage.getItem("imp");
+  let noteHTML = "";
+
   if (note == null) {
     notesObj = [];
+    noteHTML = `<p class="text-center my-5">Nothing to show! Use "Add Note" section above to add notes.</p>`;
   } else {
     notesObj = JSON.parse(note);
+    if (note.length == 2) {
+      noteHTML = `<p class="text-center my-5">Nothing to show! Use "Add Note" section above to add notes.</p>`;
+    }
   }
+  notesObj.forEach(function (notesObj, index) {
+    if (notesObj.title.length == 0) {
+      notesObj.title = "Note no.: ";
+      notesObj.title += index + 1;
+    }
 
-  let noteHTML = "";
-  // notesObj.forEach(function (element, index) {
-  //   noteHTML += `
-  //     <div class="card m-3" style="width: 18rem;">
-  //       <div class="card-body">
-  //           <h5 class="card-title">Note ${note.title}</h5>
-  //           <p class="card-text">${element}</p>
-  //           <button onclick="deleteNote(this.id)" id="${index}" class="btn btn-primary">Delete Note</button>
-  //       </div>
-  //     </div>
-  //   `;
-
-  for (let i = 0; i < notesObj.length / 2; i++) {
-    // notesObj.forEach(function (element, index) {
+    if (notesObj.imp === true) {
+      imp = "impNote";
+    } else {
+      imp = "";
+    }
     noteHTML += `
-          <div class="card m-3" style="width: 18rem;">
-            <div class="card-body">
-                <h3 class="card-title text-uppercase">${notesObj[i * 2]}</h3>
-                <p class="card-text">${notesObj[i * 2 + 1]}</p>
-                <button onclick="deleteNote(this.id)" id="${i}" class="btn btn-primary">Delete Note</button>
+            <div class="card m-3 ${imp}" style="width: 18rem;">
+              <div class="card-body">
+                  <sup class="fs-5 position-relative text-danger  text-uppercase fw-bold d-none">important</sup>
+                  <h3 class="card-title text-uppercase ">${notesObj.title}</h3>
+                  <hr>
+                  <p class="card-text">${notesObj.note}</p>
+                  <button onclick="deleteNote(this.id)" id="${index}" class="btn btn-primary">Delete Note</button>
+              </div>
             </div>
-          </div>
-        `;
-    document.getElementById("notes").innerHTML = noteHTML;
-  }
+          `;
+  });
 
-  // });
+  document.getElementById("notes").innerHTML = noteHTML;
 }
 
 // function to delete notes
 function deleteNote(index) {
-  let note = localStorage.getItem("note");
+  let note = localStorage.getItem("notes");
   if (note == null) {
     notesObj = [];
   } else {
     notesObj = JSON.parse(note);
   }
 
-  notesObj.splice(index, 2);
-  localStorage.setItem("note", JSON.stringify(notesObj));
+  notesObj.splice(index, 1);
+  localStorage.setItem("notes", JSON.stringify(notesObj));
+  localStorage;
   showNotes();
   darkMode();
-
-  if (notesObj.length == 0) {
-    location.reload();
-  }
 }
 
 function delAllNotes() {
-  window.localStorage.removeItem("note");
-  location.reload();
+  window.localStorage.removeItem("notes");
+  showNotes();
 }
 
 // searchbar function
@@ -101,83 +116,3 @@ function delAllNotes() {
 //     }
 //   });
 // });
-
-// dark mode
-function switchVal() {
-  let darkSwitch = document.getElementById("darkModeSwitch").checked;
-  let switchValue = darkSwitch.toString();
-  localStorage.setItem("switch", switchValue);
-  darkMode();
-}
-
-function darkMode() {
-  let darkSwitchValue = localStorage.getItem("switch");
-  let darkSwitch = document.getElementById("darkModeSwitch");
-
-  if (darkSwitchValue == "true") {
-    darkSwitch.checked = true;
-    darkTheme();
-  } else {
-    lightTheme();
-  }
-}
-
-function darkTheme() {
-  // body
-  document.querySelector("body").style.backgroundColor = "#121212";
-
-  // nav
-  document.querySelector("nav").classList.add("darkNav");
-
-  // input section
-  document
-    .querySelector("#inputSection")
-    .classList.add("darkContainer", "lowOpacity");
-  document
-    .querySelector("#noteTextInput")
-    .classList.add("bg-dark", "text-light");
-
-  document
-    .querySelector("#noteTitleInput")
-    .classList.add("bg-dark", "text-light");
-
-  // card container
-  document.querySelector("#noteContainer").classList.add("darkContainer");
-  document
-    .querySelector("#noteContainer h2")
-    .classList.add("pt-3", "lowOpacity");
-  let x = document.querySelectorAll("#notes .card");
-  for (let i = 0; i < x.length; i++) {
-    x[i].classList.add("bg-dark", "text-light", "darkCard");
-  }
-}
-
-function lightTheme() {
-  // body
-  document.querySelector("body").style.backgroundColor = "#ffffff";
-
-  // nav
-  document.querySelector("nav").classList.remove("darkNav");
-
-  // input section
-  document
-    .querySelector("#inputSection")
-    .classList.remove("darkContainer", "lowOpacity");
-  document
-    .querySelector("#noteTextInput")
-    .classList.remove("bg-dark", "text-light");
-
-  document
-    .querySelector("#noteTitleInput")
-    .classList.remove("bg-dark", "text-light");
-
-  // card container
-  document.querySelector("#noteContainer").classList.remove("darkContainer");
-  document
-    .querySelector("#noteContainer h2")
-    .classList.remove("pt-3", "lowOpacity");
-  let x = document.querySelectorAll("#notes .card");
-  for (let i = 0; i < x.length; i++) {
-    x[i].classList.remove("bg-dark", "text-light", "darkCard");
-  }
-}
---
+++ assets/js/dark.js
@@ -0,0 +1,91 @@
+darkMode();
+// dark mode
+function switchVal() {
+  let darkSwitch = document.getElementById("darkModeSwitch").checked;
+  let switchValue = darkSwitch.toString();
+  localStorage.setItem("switch", switchValue);
+  darkMode();
+}
+
+function darkMode() {
+  let darkSwitchValue = localStorage.getItem("switch");
+  let darkSwitch = document.getElementById("darkModeSwitch");
+
+  if (darkSwitchValue == "true") {
+    darkSwitch.checked = true;
+    darkTheme();
+  } else {
+    lightTheme();
+  }
+}
+
+function darkTheme() {
+  // body
+  document.querySelector("body").style.backgroundColor = "#121212";
+
+  // nav
+  document.querySelector("nav").classList.add("darkNav");
+
+  // input section
+  document
+    .querySelector("#inputSection")
+    .classList.add("darkContainer", "lowOpacity");
+  document
+    .querySelector("#noteTextInput")
+    .classList.add("bg-dark", "text-light");
+
+  document
+    .querySelector("#noteTitleInput")
+    .classList.add("bg-dark", "text-light");
+
+  // card container
+  document.querySelector("#noteContainer").classList.add("darkContainer");
+  document
+    .querySelector("#noteContainer h2")
+    .classList.add("pt-3", "lowOpacity");
+  let x = document.querySelectorAll("#notes .card");
+  for (let i = 0; i < x.length; i++) {
+    x[i].classList.add("bg-dark", "text-light", "darkCard");
+  }
+
+  let y = document.querySelectorAll("div.impNote .card-body sup");
+  for (let i = 0; i < y.length; i++) {
+    y[i].classList.add("bg-dark");
+  }
+}
+
+function lightTheme() {
+  // body
+  document.querySelector("body").style.backgroundColor = "#ffffff";
+
+  // nav
+  document.querySelector("nav").classList.remove("darkNav");
+
+  // input section
+  document
+    .querySelector("#inputSection")
+    .classList.remove("darkContainer", "lowOpacity");
+  document
+    .querySelector("#noteTextInput")
+    .classList.remove("bg-dark", "text-light");
+
+  document
+    .querySelector("#noteTitleInput")
+    .classList.remove("bg-dark", "text-light");
+
+  // card container
+  document.querySelector("#noteContainer").classList.remove("darkContainer");
+  document
+    .querySelector("#noteContainer h2")
+    .classList.remove("pt-3", "lowOpacity");
+
+  let x = document.querySelectorAll("#notes .card");
+  for (let i = 0; i < x.length; i++) {
+    x[i].classList.remove("bg-dark", "text-light", "darkCard");
+  }
+
+  let y = document.querySelectorAll("div.impNote .card-body sup");
+  for (let i = 0; i < y.length; i++) {
+    y[i].classList.remove("bg-dark");
+  }
+}
